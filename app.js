753758a01abefb31594fb576e04910aab0afed80
Anjas Muhammad Bangun <anjas.muhammad@wartek.belajar.id>
2024-11-23 11:45:30 +0700

    fixing responsive

--- app.js
+++ app.js
@@ -14,16 +14,7 @@ function removeClass(element, className) {
     element.classList.remove(className)
 }
 
-function toggleItem(extra) {
-    console.log(this);
-    if (this.classList.contains("active")) {
-        this.classList.remove("active");
-    } else {
-        this.classList.add("active");
-    }
-}
-
-// ========================== Top Navbar ==========================
+// ========================== Start of Top Navbar ==========================
 const topNavbar = document.querySelector('#top-navbar');
 const subNavbars = document.querySelectorAll('.sub-navbar')
 let anyActiveSubNavbar = false
@@ -38,9 +29,9 @@ window.onscroll = () => {
     }
 };
 
-// activate sub-navbar 
+//========= activate sub-navbar =========
 for (let subNavbar of subNavbars) {
-    subNavbar.addEventListener("click", toggleItem);
+    //mouse hovering
     subNavbar.addEventListener("mouseover", () => {
         topNavbar.classList.add('active');
         subNavbar.classList.add('active');
@@ -51,7 +42,30 @@ for (let subNavbar of subNavbars) {
             topNavbar.classList.remove('active');
         }
     }, false);
+
+    //user click
+    subNavbar.addEventListener("click", () => {
+        if(!subNavbar.classList.contains('mobile-active')) subNavbar.classList.add('mobile-active');
+        else subNavbar.classList.remove('mobile-active')
+    }, false);
 }
+
+// Mobile
+const toggleNavbar = document.querySelector('#toggle-navbar')
+const closeNavbar = document.querySelector('#close-navbar')
+const listNavbar = document.querySelector('#list-navbar')
+toggleNavbar.addEventListener("click", () => {
+    listNavbar.classList.remove('hidden')
+    closeNavbar.classList.remove('hidden')
+    toggleNavbar.classList.add('hidden')
+});
+
+closeNavbar.addEventListener("click", () => {
+    listNavbar.classList.add('hidden')
+    closeNavbar.classList.add('hidden')
+    toggleNavbar.classList.remove('hidden')
+});
+// ========================== End of Navbar ==========================
 
 // ======================================= Start of Education Slideshow =======================================
 const btnSlideLeft = document.querySelector('#edu-slide-left')
@@ -135,7 +149,32 @@ function updateVisibility() {
     });
 }
 // ======================================= End of Education Slideshow =======================================
-// ======================================= Start of Testimonial Carousel =======================================
+// ======================================= Start of Button Form  =======================================
+window.addEventListener('DOMContentLoaded', () => {
+    const formMsg = document.querySelector('#msg-form')
+    const inputName = document.querySelector('#msgName')
+    const inputEmail = document.querySelector('#msgEmail')
+    const inputPhone = document.querySelector('#msgPhone')
+    const inputSubject = document.querySelector('#msgSubject')
+    const inputTxtMsg = document.querySelector('#msgTxt')
+    const btnSendWA = document.querySelector('#btnSendWA')
+    const btnSendEmail = document.querySelector('#btnSendEmail')
+    const renderBodyMessage = (socialtType) => {
+        let bdyMsg = `Subject: ${inputSubject.value}\nHi, I'm  ${inputName.value},\nE-mail: ${inputEmail.value}\nPhone: ${inputPhone.value}\n\n\n${inputTxtMsg.value}`
+        if (socialtType === 'wa') bdyMsg = encodeURI(`https://api.whatsapp.com/send/?phone=6281901777719&text=${bdyMsg}&type=phone_number&app_absent=0`)
+        else bdyMsg = encodeURI(`mailto:anjasmuhamadb123@gmail.com?subject=${inputSubject.value}&body=${bdyMsg}`)
+        window.open(bdyMsg, '_blank')
+    }
+
+    btnSendWA.addEventListener('click', () => {
+        renderBodyMessage('wa')
+    });
+    btnSendEmail.addEventListener('click', () => {
+        renderBodyMessage('email')
+    });
+});
+// ======================================= End of Button Form =======================================
+/*// ======================================= Start of Testimonial Carousel =======================================
 class Carousel {
     constructor(el) {
         this.el = el;
@@ -327,30 +366,4 @@ const el = document.querySelector('.carousel');
 const exampleCarousel = new Carousel(el);
 // Setup carousel and methods
 exampleCarousel.mounted();
-
-// ======================================= End of Testimonial Carousel =======================================
-// ======================================= Start of Button Form  =======================================
-window.addEventListener('DOMContentLoaded', () => {
-    const formMsg = document.querySelector('#msg-form')
-    const inputName = document.querySelector('#msgName')
-    const inputEmail = document.querySelector('#msgEmail')
-    const inputPhone = document.querySelector('#msgPhone')
-    const inputSubject = document.querySelector('#msgSubject')
-    const inputTxtMsg = document.querySelector('#msgTxt')
-    const btnSendWA = document.querySelector('#btnSendWA')
-    const btnSendEmail = document.querySelector('#btnSendEmail')
-    const renderBodyMessage = (socialtType) => {
-        let bdyMsg = `Subject: ${inputSubject.value}\nHi, I'm  ${inputName.value},\nE-mail: ${inputEmail.value}\nPhone: ${inputPhone.value}\n\n\n${inputTxtMsg.value}`
-        if (socialtType === 'wa') bdyMsg = encodeURI(`https://api.whatsapp.com/send/?phone=6281901777719&text=${bdyMsg}&type=phone_number&app_absent=0`)
-        else bdyMsg = encodeURI(`mailto:anjasmuhamadb123@gmail.com?subject=${inputSubject.value}&body=${bdyMsg}`)
-        window.open(bdyMsg, '_blank')
-    }
-
-    btnSendWA.addEventListener('click', () => {
-        renderBodyMessage('wa')
-    });
-    btnSendEmail.addEventListener('click', () => {
-        renderBodyMessage('email')
-    });
-});
-// ======================================= End of Button Form =======================================
+// ======================================= End of Testimonial Carousel =======================================*/
